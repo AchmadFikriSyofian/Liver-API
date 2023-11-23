1548cca88b0113f4f4e8f6dc4369672afba07445
unknown <Hp@NgabPikri>
2023-11-23 21:31:50 +0700

    add feature course list with pagination

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
+const {getAllCourse} = require('../controllers/course.controllers');
 
-
-
-
+router.get('/list', getAllCourse);
 
 module.exports = router;
