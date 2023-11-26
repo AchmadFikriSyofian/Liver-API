31ab7a106feee74d1ed5a0c1eb73b9f7f3af1ae1
unknown <Hp@NgabPikri>
2023-11-26 14:13:50 +0700

    add swagger file for documentation api

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yaml');
+const cors = require('cors');
 const {PORT = 3000} = process.env;
+
+const fs = require("fs");
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const courseRouter = require('./routes/course.routes');
 app.use('/api/v1/course', courseRouter); 
