5ce470f19ee2f35f11b3989e409f2333a1eef016
nuruldwia <nuruldwia@students.unnes.ac.id>
2023-12-01 13:00:30 +0700

    update course detail: add duration

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const app = express();
 const morgan = require('morgan');
 const YAML = require('yaml');
 const cors  = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yaml');
-const cors = require('cors');
+// const swaggerUi = require('swagger-ui-express');
 const {PORT = 3000} = process.env;
 
 const fs = require("fs");
@@ -16,7 +14,7 @@ const swaggerDocument = YAML.parse(file);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const courseRouter = require('./routes/course.routes');
 app.use('/api/v1/course', courseRouter); 
