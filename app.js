75791ab7448c7a544dfe56ce952bf881c66ea7fc
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2023-12-05 15:29:59 +0700

    adjust script for elephant sql

--- app.js
+++ app.js
@@ -29,5 +29,23 @@ app.use('/api/v1/course', courseRouter);
 const categoriesRouter = require('./routes/categories.routes');
 app.use('/api/v1/categories', categoriesRouter);
 
+// Elephant SQL
+const pg = require('pg');
+
+const conString = "postgres://xngbaqnj:kJj9JRUNN2QPakSwclHMc-DxFW5sx9dZ@rain.db.elephantsql.com/xngbaqnj";
+const client = new pg.Client(conString);
+client.connect(function(err){
+    if(err){
+        return console.log('could not connect to postgres', err);
+    }
+    client.query('SELECT NOW() AS "theTime"', function(err, result) {
+        if(err){
+            return console.log('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+        client.end();
+    });
+});
+
 
 app.listen(PORT, () => console.log('Listening on Port', PORT));
