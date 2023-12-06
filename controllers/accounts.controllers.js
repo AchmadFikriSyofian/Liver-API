bbaf67aa6146394b69803253b7b7bc1622b06b9f
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2023-12-06 21:53:52 +0700

    add feature change password

--- controllers/accounts.controllers.js
+++ controllers/accounts.controllers.js
@@ -79,7 +79,7 @@ module.exports = {
                     message: 'Bad Request',
                     err: 'Password must contain at least one digit, special character, lowercase, uppercase, and at least 8 character long.',
                     data: null
-                })
+                });
             }
 
             const encryptedNewPassword = await bcrypt.hash(newPassword, 10);
@@ -97,6 +97,31 @@ module.exports = {
                 data: updatePassword
             })
         } catch(err){
+            next(err);
+        }
+    },
+
+    payment_history: async (req, res, next) => {
+        try{
+            let {id} = req.params;
+            
+            const enrollment = await prisma.enrollments.findUnique({where: {id: Number(id)}});
+            if(!enrollment){
+                return res.status(404).json({
+                    status: false,
+                    message: 'Not Found',
+                    err: 'User id is not Exist',
+                    data: null
+                });
+            }
+
+            res.status(200).json({
+                status: true,
+                message: 'OK',
+                err: null,
+                data: {enrollment}
+            })
+        }catch(err){
             next(err);
         }
     }
--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const {updateProfile, updatePassword} = require('../controllers/accounts.controllers');
+const {updateProfile, updatePassword, payment_history} = require('../controllers/accounts.controllers');
 const {image} = require('../libs/multer');
 
 router.put('/updateprofile/:id', image.single('image'), updateProfile);
 router.put('/updatepassword/:id', updatePassword);
+router.get('/paymenthistory/:id', payment_history);
 
 module.exports = router;
