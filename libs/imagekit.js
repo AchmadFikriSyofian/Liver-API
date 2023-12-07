c44a0f6c2f4b069b8e8caabf5b8fde942db7e245
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2023-12-07 23:48:33 +0700

    adjust imagekit.js

--- libs/imagekit.js
+++ libs/imagekit.js
@@ -1,21 +1,8 @@
-// const Imagekit = require('imagekit');
-// const {IMAGEKIT_URL_ENDPOINT, IMAGEKIT_PUBLIC_KEY, IMAGEKIT_PRIVATE_KEY} = process.env;
-
-// module.exports = new Imagekit({
-//     publicKey: IMAGEKIT_PUBLIC_KEY,
-//     privateKey: IMAGEKIT_PRIVATE_KEY,
-//     urlEndpoint: IMAGEKIT_URL_ENDPOINT
-// });
-
-// SDK initialization
-
-var ImageKit = require("imagekit");
+const Imagekit = require('imagekit');
 const {IMAGEKIT_URL_ENDPOINT, IMAGEKIT_PUBLIC_KEY, IMAGEKIT_PRIVATE_KEY} = process.env;
 
-var imagekit = new ImageKit({
-    publicKey : IMAGEKIT_PUBLIC_KEY,
-    privateKey : IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint : IMAGEKIT_URL_ENDPOINT
+module.exports = new Imagekit({
+    publicKey: "public_zxVyhwhgp5/sRFNzWwqYPHw/C/M=",
+    privateKey: "private_Fe+KLll7Jf1Zw4nkKXlBVDtCp6E=",
+    urlEndpoint: "https://ik.imagekit.io/livercourse"
 });
-
-module.exports = imagekit
