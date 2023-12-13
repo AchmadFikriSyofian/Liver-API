915ae6f32c5ae5653e7fe51c8fb083d7e986d96c
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2023-12-13 22:16:11 +0700

    update swagger for Course Detail

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { register, verify, newOTP, login, forgotPassword, resetPassword  } = require('../controllers/auth.controllers');
 
 router.post('/forget-password', forgotPassword);
-router.put('/reset-password/', resetPassword);
+router.put('/updatepass/', resetPassword);
 router.post('/register', register);
 router.post('/verify', verify);
 router.put('/verify/newOtp', newOTP);
