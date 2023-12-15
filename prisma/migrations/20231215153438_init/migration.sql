8c92309fda73f15b28bd507c665b757915e419c9
Tatang Romadhona <romadhonatatang@gmail.com>
2023-12-15 22:34:55 +0700

    feat: fix db connection

--- prisma/migrations/20231215153438_init/migration.sql
+++ prisma/migrations/20231215153438_init/migration.sql
@@ -4,6 +4,12 @@ CREATE TYPE "Type" AS ENUM ('isFree', 'isPremium');
 -- CreateEnum
 CREATE TYPE "Level" AS ENUM ('Beginner', 'Intermediate', 'Advanced');
 
+-- CreateEnum
+CREATE TYPE "StatusPembayaran" AS ENUM ('belumBayar', 'sudahBayar');
+
+-- CreateEnum
+CREATE TYPE "MetodePembayaran" AS ENUM ('bankTransfer', 'creditCard');
+
 -- CreateTable
 CREATE TABLE "Users" (
     "id" SERIAL NOT NULL,
@@ -18,6 +24,16 @@ CREATE TABLE "Users" (
     "is_admin" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Otp" (
+    "id" SERIAL NOT NULL,
+    "kode_otp" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "expiredAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Otp_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -44,10 +60,13 @@ CREATE TABLE "Courses" (
     "desc" TEXT NOT NULL,
     "price" TEXT NOT NULL,
     "level" "Level" NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "rating" DOUBLE PRECISION NOT NULL,
     "type" "Type" NOT NULL DEFAULT 'isFree',
     "image" TEXT,
-    "duration" INTEGER NOT NULL,
+    "intended_for" TEXT,
+    "total_lesson" INTEGER,
+    "total_duration" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Courses_pkey" PRIMARY KEY ("id")
 );
@@ -65,6 +84,7 @@ CREATE TABLE "MentorsOnCourses" (
 CREATE TABLE "CategoriesOnCourses" (
     "category_id" INTEGER NOT NULL,
     "course_id" INTEGER NOT NULL,
+    "assignedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "CategoriesOnCourses_pkey" PRIMARY KEY ("category_id","course_id")
 );
@@ -84,6 +104,7 @@ CREATE TABLE "Lessons" (
     "name" TEXT NOT NULL,
     "video" TEXT NOT NULL,
     "desc" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
     "chapter_id" INTEGER NOT NULL,
 
     CONSTRAINT "Lessons_pkey" PRIMARY KEY ("id")
@@ -91,12 +112,27 @@ CREATE TABLE "Lessons" (
 
 -- CreateTable
 CREATE TABLE "Enrollments" (
-    "id" SERIAL NOT NULL,
-    "payment" TEXT,
+    "id" INTEGER NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "statusPembayaran" "StatusPembayaran" NOT NULL DEFAULT 'belumBayar',
+    "metodePembayaran" "MetodePembayaran" NOT NULL,
+    "tanggalBayar" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "user_id" INTEGER NOT NULL,
     "course_id_enrollment" INTEGER NOT NULL,
 
     CONSTRAINT "Enrollments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CreditCard" (
+    "id" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "cvv" INTEGER NOT NULL,
+    "expired" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "CreditCard_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -106,16 +142,45 @@ CREATE TABLE "CoursePromos" (
     "discount_percentage" INTEGER NOT NULL,
     "start_date" TIMESTAMP(3) NOT NULL,
     "end_date" TIMESTAMP(3) NOT NULL,
-    "course_id_promo" INTEGER NOT NULL,
 
     CONSTRAINT "CoursePromos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notifications" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiredAt" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER,
+
+    CONSTRAINT "Notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CoursePromosToCourses" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "Otp_kode_otp_key" ON "Otp"("kode_otp");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "CoursePromos_promo_code_key" ON "CoursePromos"("promo_code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CoursePromosToCourses_AB_unique" ON "_CoursePromosToCourses"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CoursePromosToCourses_B_index" ON "_CoursePromosToCourses"("B");
+
+-- AddForeignKey
+ALTER TABLE "Otp" ADD CONSTRAINT "Otp_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "MentorsOnCourses" ADD CONSTRAINT "MentorsOnCourses_mentor_id_fkey" FOREIGN KEY ("mentor_id") REFERENCES "Mentors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -142,4 +207,13 @@ ALTER TABLE "Enrollments" ADD CONSTRAINT "Enrollments_user_id_fkey" FOREIGN KEY 
 ALTER TABLE "Enrollments" ADD CONSTRAINT "Enrollments_course_id_enrollment_fkey" FOREIGN KEY ("course_id_enrollment") REFERENCES "Courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CoursePromos" ADD CONSTRAINT "CoursePromos_course_id_promo_fkey" FOREIGN KEY ("course_id_promo") REFERENCES "Courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CreditCard" ADD CONSTRAINT "CreditCard_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notifications" ADD CONSTRAINT "Notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CoursePromosToCourses" ADD CONSTRAINT "_CoursePromosToCourses_A_fkey" FOREIGN KEY ("A") REFERENCES "CoursePromos"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CoursePromosToCourses" ADD CONSTRAINT "_CoursePromosToCourses_B_fkey" FOREIGN KEY ("B") REFERENCES "Courses"("id") ON DELETE CASCADE ON UPDATE CASCADE;
