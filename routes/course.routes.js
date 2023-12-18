f0b5b05658f6eec09d9055972d7fb63ab832874b
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2023-12-19 05:08:06 +0700

    update authorization and documentation

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -4,7 +4,7 @@ const {restrict} = require('../middlewares/auth.middlewares');
 
 router.get('/list', restrict, getAllCourse);
 router.get('/populer/:id', restrict, getCoursePopuler);
-router.get('/populerAll', restrict, getPopulerAll);
+router.get('/populerall', restrict, getPopulerAll);
 router.get('/details/:id', restrict, getDetailCourse);
 router.get('/premium', restrict, getPremiumCourse);
 router.get('/free', restrict, getFreeCourse);
