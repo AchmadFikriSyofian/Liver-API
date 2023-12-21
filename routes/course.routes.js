0495b14bc72198afa6661a4d2109f82a29b73b61
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2023-12-22 00:12:52 +0700

    add endpoint update is done

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const {getAllCourse, getCoursePopuler, getPopulerAll, getDetailCourse, search, filter, getByEnrollment, getPremiumCourse, getFreeCourse, getAllFreePrem} = require('../controllers/course.controllers');
+const {getAllCourse, getCoursePopuler, getPopulerAll, getDetailCourse, updateIsDone, search, filter, getByEnrollment, getPremiumCourse, getFreeCourse, getAllFreePrem} = require('../controllers/course.controllers');
 const {restrict} = require('../middlewares/auth.middlewares');
 
 router.get('/list', restrict, getAllCourse);
 router.get('/populer/:id', getCoursePopuler);
 router.get('/populerall', getPopulerAll);
 router.get('/details/:id', restrict, getDetailCourse);
+router.put('/updateisdone', restrict, updateIsDone);
 router.get('/premium', restrict, getPremiumCourse);
 router.get('/free', restrict, getFreeCourse);
 router.get('/search/', search);
