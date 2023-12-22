33a4b9e53910595c398dddfa495c9263b2b8024b
Achmad Fikri Syofian <91151241+pikriPnyaPemula@users.noreply.github.com>
2023-12-22 10:04:09 +0700

    update endpoint update profile

--- controllers/accounts.controllers.js
+++ controllers/accounts.controllers.js
@@ -9,7 +9,7 @@ module.exports = {
     updateProfile: async(req, res, next) => {
         try{
             let {id} = req.user;
-            let {foto_profile, name, no_hp, country, city} = req.body;
+            let {name, no_hp, country, city} = req.body;
 
             const userExist = await prisma.users.findUnique({where: {id: Number(id)}});
             if(!userExist){
@@ -21,17 +21,29 @@ module.exports = {
                 });
             }
 
-            let strFile = req.file.buffer.toString('base64');
+            let updateFields = {
+                name,
+                email: userExist.email,
+                password: userExist.password,
+                no_hp,
+                country,
+                city
+            }
 
-            let {url} = await imagekit.upload({
-                fileName: Date.now() + path.extname(req.file.originalname),
-                file: strFile
-            });
+            if(req.file){
+                let strFile = req.file.buffer.toString('base64');
+    
+                let {url} = await imagekit.upload({
+                    fileName: Date.now() + path.extname(req.file.originalname),
+                    file: strFile
+                });
+                updateFields.foto_profile = url;
+            }
 
             let updateOperation = await prisma.users.upsert({
                 where: {id: Number(id)},
-                update: {foto_profile: url, name, email: userExist.email, password: userExist.password, no_hp, country, city},
-                create: {id: Number(id), foto_profile: url, name, email: userExist.email, password: userExist.password, no_hp, country, city}
+                update: updateFields,
+                create: {id: Number(id), ...updateFields}
             });
 
             return res.status(200).json({
